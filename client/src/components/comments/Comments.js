542bbed491694ad8d05a5da542984f8804b4771a
wheninseattle <weston.norwood@gmail.com>
2022-05-30 21:22:21 -0700

    comment tree function!

--- client/src/components/comments/Comments.js
+++ client/src/components/comments/Comments.js
@@ -5,39 +5,52 @@ import CommentForm from "./CommentForm";
 import AuthContext from "../../context/auth/authContext";
 
 const Comments = () => {
+  // Initialize and destructure List Context
   const listContext = useContext(ListContext);
   const { comments } = listContext;
+
+  // Initialize and destructure Auth Context
   const authContext = useContext(AuthContext);
   const { user } = authContext;
-  console.table(comments);
-  const rootComments = comments.filter((comment) => comment.parents.length < 2);
-  const thread = [];
-  rootComments.forEach((rootComment) => {
-    const replies = comments
-      .filter((comment) => comment.parents[0] === rootComment.parents[0])
-      .sort((a, b) => b.parents.length - a.parents.length);
-    replies.forEach(reply => { 
-      if (reply.parents.length > 1) {
-        let parentComment = reply.parents[reply.parents.length - 2];
-        
-        if (parentComment) {
-          if (!parentComment.replies) {
-            parentComment.replies = [];
-          }
-          parentComment.replies.push(reply)
-          thread.push(parentComment)
-        }
-      }
-      
+
+  const buildCommentTree = (comments = []) => {
+    const sortedComments = comments.sort(
+      (a, b) => b.parents.length - a.parents.length
+    );
+    console.table(sortedComments)
+
+    const commentMap = Object.create(null); //Initialize map
+    // Create object for each comment in array, add childNodes property
+    sortedComments.forEach((comment) => {
+      commentMap[comment._id] = { ...comment, childNodes: [] };
     });
-    console.table(replies);
-  });
-  console.table(thread);
+    console.table(commentMap)
+    const commentTree = []; // Initialize output array
+    console.table(commentTree)
+    sortedComments.forEach((comment) => {
+      if (comment.parents.length > 1) {
+        // If comment has parents push comment into childNodes property of parent object
+        // Note: MongoDB comment structure lists all ancestors of object as parents
+        // The final parents entry is current object
+        commentMap[comment.parents[comment.parents.length - 2]].childNodes.push(
+          commentMap[comment._id]
+          );
+        } else {
+          // If only parent is self then this is root comment
+          commentTree.push(commentMap[comment._id]);
+        }
+      });
+      console.table(commentMap)
+      console.table(commentTree)
+    return commentTree;
+  };
+  buildCommentTree(comments)
   return (
-    <div>
+    
+    <div className="comment-thread">
       {user && <CommentForm currentUser={user} />}
       {comments.length &&
-        comments.map((comment) => (
+        buildCommentTree(comments).map((comment) => (
           <Comment key={comment._id} currentUser={user} comment={comment} />
         ))}
     </div>
