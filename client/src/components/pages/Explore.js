359587063747dcaf746ee2118cc8bcb63c1e2197
wheninseattle <weston.norwood@gmail.com>
2022-05-30 21:32:53 -0700

    Clean up

--- client/src/components/pages/Explore.js
+++ client/src/components/pages/Explore.js
@@ -1,27 +1,22 @@
-import React, {useContext, useEffect} from "react";
+import React, { useContext, useEffect } from "react";
 import ListContext from "../../context/list/listContext";
-import Lists from "../../components/list/Lists"
+import Lists from "../../components/list/Lists";
 
 const Explore = () => {
-
-    // Initialize and destructure context
-    const listContext = useContext(ListContext);
-    const {getPublicLists,communityLists} = listContext;
-
-    // const onClick = () => {
-    //     getPublicLists();
-    // }
-
-    useEffect(()=> {
-      getPublicLists();
-    }, [])
+  // Initialize and destructure context
+  const listContext = useContext(ListContext);
+  const { getPublicLists, communityLists } = listContext;
+  
+  // Retrieve public lists
+  useEffect(() => {
+    getPublicLists();
+  }, []);
 
   return (
     <div className="all-center">
-        {(communityLists.length) &&
-        <Lists lists={communityLists}/>}
+      {communityLists.length && <Lists lists={communityLists} />}
     </div>
-  )
-}
+  );
+};
 
-export default Explore
+export default Explore;
