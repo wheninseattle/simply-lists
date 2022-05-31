a4e51cddd745a9d9c91372934996d6ba2db42c17
wheninseattle <weston.norwood@gmail.com>
2022-05-30 21:20:27 -0700

    Created recursive comment render for nested comments

--- client/src/components/comments/Comment.js
+++ client/src/components/comments/Comment.js
@@ -1,41 +1,54 @@
 import React, { useState } from "react";
 import { timeAgo } from "../../utils/timeAgo";
 import CommentForm from "./CommentForm";
+import { IconReply } from "../icons/IconReply";
 
-const Comment = ({ comment,currentUser }) => {
+const Comment = ({ comment, currentUser }) => {
   const [state, setState] = useState({
     showReply: false,
   });
 
-  const nestedComments = (comment.children || []).map((comment) => {
-    return <Comment key={comment._id} comment={comment} />;
+  const nestedComments = (comment.childNodes || []).map((childNode) => {
+    console.table(childNode);
+    return (
+      <Comment
+        key={comment._id}
+        comment={childNode}
+        className="comment-replies"
+      />
+    );
   });
+
   const onReply = () => {
     setState({
       ...state,
-      showReply: !state.showReply
-    })
+      showReply: !state.showReply,
+    });
   };
 
   const { username, message, date } = comment;
   return (
-    <div className="my-1">
-      <div>
-        <p>{message}</p>
-        <span>
-          <strong>{username} | </strong>
-        </span>
-        {timeAgo(date)}
-        {nestedComments}
+    <div className="comment" id={comment._id}>
+      <a href={`#${comment._id}`} className="comment-border-link">
+        <span className="sr-only">Jump to comment parent</span>
+      </a>
+      <div className="comment-header">
+        <div className="comment-info">
+          <pre className="comment-user">{username}</pre>
+          <pre> | </pre>
+          {timeAgo(date)}
+        </div>
       </div>
+      <div className="comment-body"></div>
+      
+      <p>{message}</p>
       <div>
-        <button className="btn btn-primary" onClick={onReply}>
-          Reply
-        </button>
+        <IconReply className="btn-reply" onClick={onReply} />
       </div>
       {state.showReply && (
-          <CommentForm parent={comment} currentUser={currentUser} />
+        <CommentForm parent={comment} currentUser={currentUser} />
       )}
+      <div>{nestedComments}</div>
     </div>
   );
 };
