89f20c810f6c305b50508afecba431ca2af1246d
wheninseattle <weston.norwood@gmail.com>
2022-06-11 18:15:22 -0700

    Fix authorization error with fetching public list items. Removed authentication. @todo - add new route or optional authentication when creating public/private list functionality

--- routes/listItems.js
+++ routes/listItems.js
@@ -6,38 +6,33 @@ const { check, validationResult } = require("express-validator");
 // const User = require("../models/User");
 const List = require("../models/List");
 
-
 // @route    POST  api/listItems
 // @desc     Create a new list item
 // @access   Public
-router.post(
-  "/",
-  auth,
-  async (req, res) => {
-    const { name,description,listId } = req.body;
-    try {
-      const list = await List.findById(listId);
-      const listItems = list.listItems;
-      listItems.push({
-        name: name,
-        description: description,
-        user: req.userlid,
-        list: listId
-      })
-      const updated = await list.save();
-      const newItem = updated.listItems[(updated.listItems.length-1)]
-      res.json(newItem);
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send("Server Error");
-    }
+router.post("/", auth, async (req, res) => {
+  const { name, description, listId } = req.body;
+  try {
+    const list = await List.findById(listId);
+    const listItems = list.listItems;
+    listItems.push({
+      name: name,
+      description: description,
+      user: req.userlid,
+      list: listId,
+    });
+    const updated = await list.save();
+    const newItem = updated.listItems[updated.listItems.length - 1];
+    res.json(newItem);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
   }
-);
+});
 
 // @route    Get  api/listsItems
-// @desc     Get private lists
+// @desc     Get list items for public list
 // @access   Public
-router.get("/:id", auth, async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
     const list = await List.findById(req.params.id);
     const listItems = list.listItems;
@@ -61,10 +56,10 @@ router.put("/:listId/:itemId", auth, async (req, res) => {
       return res.status(401).json({ msg: "Not authorized" });
     }
     let listItem = list.listItems.id(req.params.itemId);
-    if(name) listItem.name = name;
-    if(description) listItem.description = description;
+    if (name) listItem.name = name;
+    if (description) listItem.description = description;
     let update = list.save();
-    res.json(update)
+    res.json(update);
   } catch (err) {}
   console.error(err.message);
   res.status(500).send("Server Error");
@@ -74,25 +69,28 @@ router.put("/:listId/:itemId", auth, async (req, res) => {
 // @desc     Delete a list
 // @access   Private
 router.delete("/:listId/:itemId", auth, async (req, res) => {
-    try {
-        let list = await List.findByIdAndUpdate(req.params.listId,{
-          $pull: {listItems: {_id: req.params.itemId}}
-        },{
-          new: true,
-          runValidators: true
-        });
-    
-        if (!list) return res.status(400).json({ msg: "List not found" });
-        // Make sure user owns list
-        if (list.user.toString() != req.user.id) {
-          return res.status(401).json({ msg: "Not authorized" });
-        }
-        res.json({msg:'List Item Removed',id:req.params.itemId});
-      } catch (err) {
-          console.error(err.message);
-          res.status(500).send("Server Error");
-
+  try {
+    let list = await List.findByIdAndUpdate(
+      req.params.listId,
+      {
+        $pull: { listItems: { _id: req.params.itemId } },
+      },
+      {
+        new: true,
+        runValidators: true,
       }
-    });
+    );
+
+    if (!list) return res.status(400).json({ msg: "List not found" });
+    // Make sure user owns list
+    if (list.user.toString() != req.user.id) {
+      return res.status(401).json({ msg: "Not authorized" });
+    }
+    res.json({ msg: "List Item Removed", id: req.params.itemId });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
